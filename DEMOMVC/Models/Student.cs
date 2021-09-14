0454283fda06c01044ec7631eeaa37acbc7450bc
lehaidang <H-SKY@DESKTOP-NBVSKA9>
2021-09-14 14:03:06 +0700

    Add project files.

---
+++ DEMOMVC/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace DEMOMVC.Models
+{
+    public class Student
+    {[Key]
+        public String StudentID { get; set; }
+        public String StudentName { get; set; }
+    }
+}
